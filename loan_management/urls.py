9aa12656b1f06e448f1b55144a0c1183af0cdc6d
Raghav Gandhi <rgandhi3352@gmail.com>
2024-10-07 02:07:11 +0530

    update url file

--- loan_management/urls.py
+++ loan_management/urls.py
@@ -17,6 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import (
+    RegisterUserView,
+    ApplyLoanView,
+    MakePaymentView,
+    GetStatementView,
+)
+
+urlpatterns = [
+    path('api/register-user/', RegisterUserView.as_view(), name='register_user'),
+    path('api/apply-loan/', ApplyLoanView.as_view(), name='apply_loan'),
+    path('api/make-payment/', MakePaymentView.as_view(), name='make_payment'),
+    path('api/get-statement/', GetStatementView.as_view(), name='get_statement'),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
